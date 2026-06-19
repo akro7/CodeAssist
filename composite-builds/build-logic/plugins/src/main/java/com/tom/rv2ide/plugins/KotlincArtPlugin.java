fdbe34b9e106db69eafd87c79727588de4373464
AKRO <habiba31245@gmail.com>
2026-06-19 16:16:24 +0300

    Convert KotlincArtPlugin from Kotlin to Java

---
+++ composite-builds/build-logic/plugins/src/main/java/com/tom/rv2ide/plugins/KotlincArtPlugin.java
@@ -0,0 +1,12 @@
+package com.tom.rv2ide.plugins;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class KotlincArtPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project target) {
+        // No-op stub: plugin exists so the id resolves.
+        // AsmClassVisitorFactory passes will be added here when needed.
+    }
+}
