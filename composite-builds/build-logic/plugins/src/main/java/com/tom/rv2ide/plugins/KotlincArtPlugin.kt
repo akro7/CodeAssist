765337d243e671f70b431be09e55fbd71d714be3
AKRO <habiba31245@gmail.com>
2026-06-19 17:08:44 +0300

    Convert KotlincArtPlugin to Kotlin
    Refactor KotlincArtPlugin from Java to Kotlin.

---
+++ composite-builds/build-logic/plugins/src/main/java/com/tom/rv2ide/plugins/KotlincArtPlugin.kt
@@ -0,0 +1,8 @@
+package com.tom.rv2ide.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KotlincArtPlugin : Plugin<Project> {
+    override fun apply(target: Project) {}
+}
